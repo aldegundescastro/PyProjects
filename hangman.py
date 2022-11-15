c7f69e9aa8e96d055d4cdf6db7e13451b7e0db6f
AldegundesNC <54942000+AldegundesNC@users.noreply.github.com>
2022-11-15 20:19:49 -0300

    jogo da forca 2

--- hangman.py
+++ hangman.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Hangman Game (Jogo da Forca)
 # Programação Orientada a Objetos
 
@@ -68,24 +69,37 @@ O   |
 
 # Classe
 class Hangman:
-
+    i_board = 0
     # Método Construtor
     def __init__(self, word):
+        self.word = word
+        self.ind_board = 0
 
     # Método para adivinhar a letra
     def guess(self, letter):
+        self.letter = letter
+        # verifica se a letra degitada está presente na palavra
+        if self.word.find(self.letter) > 0:
+            print("letra encontrada !")
+        else:
+            self.ind_board += 1
+            print("letra não encontrada !")
+
+
 
     # Método para verificar se o jogo terminou
     def hangman_over(self):
-
+        print("hangman_over")
     # Método para verificar se o jogador venceu
     def hangman_won(self):
-
+        print("hangman_won")
     # Método para não mostrar a letra no board
     def hide_word(self):
+        print("hide_word")
 
     # Método para checar o status do game e imprimir o board na tela
     def print_game_status(self):
+        print(board[self.ind_board])
 
 
 # Função para ler uma palavra de forma aleatória do banco de palavras
@@ -100,10 +114,16 @@ def main():
     # Objeto
     game = Hangman(rand_word())
 
-    # Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
-
-    # Verifica o status do jogo
-    game.print_game_status()
+    # cont referente ao número de chace (tamanho da palavra)
+    cont = 0
+    while cont <= len(game.word):
+        # Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
+        # Verifica o status do jogo
+        game.print_game_status()
+        letra = input("Digite uma letra: ")
+        game.guess(letra)
+        cont += 1
+        # print(letra)
 
     # De acordo com o status, imprime mensagem na tela para o usuário
     if game.hangman_won():
